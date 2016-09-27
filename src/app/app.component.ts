c97ebe7767dd4c13f4c92519bfa29f2b0061f13d
Pichchan9k <pichchanok.n@hotmail.com>
2016-09-27 10:22:17 +0800

    update css and date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,9 @@ export class AppComponent {
       new OrderItem('Samsung Note7',1,2490),
       new OrderItem('MacBook',1,4900),
       new OrderItem('Ipad Mini',2,1450)
-    ], new Date("2015-12-04"));
+    ]
+    // , new Date("2015-12-04")
+    );
     
   }
   
